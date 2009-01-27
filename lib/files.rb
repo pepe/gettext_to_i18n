6ff726b349dcc3752ff898fc010f579567e509b0
Josef Pospíšil <pepe@gravastar.cz>
2009-01-27 13:29:47 +0100

    redone files through dynamical calling

--- lib/files.rb
+++ lib/files.rb
@@ -2,50 +2,24 @@
 module GettextToI18n
   class Files
     
-    # all files that contain some gettext methods
-    def self.all_files
-      self.controller_files + self.view_files + self.helper_files + self.lib_files + self.model_files
-    end
-    
-    # All controller files
-    def self.controller_files
-      self.get_files('app/controllers', '*.rb')
-    end
-    
-    # All view files
-    def self.view_files
-      self.get_files('app/views', '**/*.{erb,builder,rhtml}')
-    end
-    
-    # All view files
-    def self.lib_files
-      self.get_files('lib', '**/*.rb')
-    end
-  
-    
-    # All view files
-    def self.helper_files
-      self.get_files('app/helpers', '*.rb')
-    end
-    
-    # All view files
-    def self.model_files
-      self.get_files('app/models', '*.rb')
-    end
-    
+    PATH_PAIRS = {
+      'all' => ['app/**', '*.{erb,builder,rhtml}'],
+      'controller' => ['app/controllers', '*.rb'],
+      'view' => ['app/views', '**/*.{erb,builder,rhtml}'],
+      'lib' => ['lib', '**/*.rb'],
+      'helper' => ['app/helpers', '*.rb'],
+      'model' => ['app/models', '*.rb']
+    }.freeze
+    CONTEXTS = PATH_PAIRS.keys.freeze
+    CONTEXTS_REGEX = Regexp.new("(%s)_files" % CONTEXTS.join('|')).freeze
     
     private 
-    
-    def self.chdir
-      
-      
-      Dir.chdir(RAILS_ROOT)
+    # dynamicaly generates files arrays from path pairs
+    def self.method_missing(method_id)
+      method_id.to_s =~ CONTEXTS_REGEX
+      Dir[File.join(RAILS_ROOT, PATH_PAIRS[$1])]
+
     end
 
-    # All files we need to walk
-    def self.get_files(filter = '**', types='*.{erb,rb}')
-      self.chdir
-      Dir.glob("#{filter}/#{types}")
-    end
   end
 end
